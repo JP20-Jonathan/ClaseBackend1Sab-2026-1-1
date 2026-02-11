a3ca5199a64a72a4107c6d33911d8f18e43071b0
Jonathan Jurado Pineda <jojupi12@gmail.com>
2026-02-10 23:10:55 -0500

    fix US-002 customers created

--- src/main/java/CustomerSystem/SellingSystem.java
+++ src/main/java/CustomerSystem/SellingSystem.java
@@ -1,27 +1,50 @@
 package CustomerSystem;
-
 import java.util.Scanner;
 
 public class SellingSystem {
 
     static Scanner sc = new Scanner(System.in);
-
     // Product Atributes
     static int id;
     static String description;
     static double price;
     static int quantity;
     static boolean state;
+    // Customer Atributes
+    static int idCustomer;
+    static String nameCustomer;
+    static String apellido;
+    static String direccion;
+    static long telefono;
+    static String correo;
 
+
+    // Aquí comienza a leer y ejecutar el codigo debido al Main
+    // Aquí coloco las funciones que se ejecutaran con la clase SellingSystem
     public static void main(String[] args) {
+
         createProducts();
         getProducts();
+        System.out.println("Ingrese el ID del producto a actualizar");
         int id = sc.nextInt();
         sc.nextLine();
         updateProducts(id);
+        System.out.println("Ingrese el ID que desea eliminar");
+        id = sc.nextInt();
+        deleteProduct(id);
+        createCustomers();
+        getCustomers();
+        System.out.println("Ingrese el ID del Cliente a actualizar");
+        idCustomer = sc.nextInt();
+        sc.nextLine();
+        updateCustomers(idCustomer);
+        System.out.println("Ingrese el ID del Cliente a eliminar");
+        idCustomer = sc.nextInt();
+        sc.nextLine();
+        deleteCustomers(idCustomer);
     }
-        // Class Methods
-    // Metodo Crear
+
+    // Metodo Crear productos
     public static void createProducts() {
 
             System.out.println("Ingrese el ID del producto");
@@ -36,12 +59,12 @@ public class SellingSystem {
             quantity = sc.nextInt();
             sc.nextLine();
             System.out.println("Ingrese el estado del producto");
-            sc.nextBoolean();
+            state =sc.nextBoolean();
         }
 
-
-        // Metodo de Mostrar
+        // Metodo de Mostrar productos
         public static void getProducts(){
+
             System.out.println("ID: " + id + "\n" +
                     "Description: " + description + "\n" +
                     "Price: " + price + "\n" +
@@ -49,7 +72,7 @@ public class SellingSystem {
                     "State: " + state + "\n");
         }
 
-        // Metodo de Actualizar
+        // Metodo de Actualizar productos
     public static void updateProducts(int findId) {
 
         if (findId == id) {
@@ -72,21 +95,99 @@ public class SellingSystem {
         }
     }
 
-
-    // Metodo de Eliminar
+    // Metodo de Eliminar productos
     public static void deleteProduct(int deleteId){
+
         if (deleteId == id){
-            id = 0;
-            description = " ";
-            price = 0.0;
-            quantity = 0;
-            state = false;
+            System.out.println(
+                    "            id = 0; \n" +
+                    "            description = \" \";\n" +
+                    "            price = 0.0;\n" +
+                    "            quantity = 0;\n" +
+                    "            state = false);");
+
+            System.out.println("El ID fue eliminado Correctamente");
         } else {
-            System.out.println( "ID: " + id + "ID no encontrado");
+            System.out.println( "ID: " + id + " no encontrado");
         }
     }
-    
 
-    // Metodos Cliente
+
+
+    // CLIENTES
+
+
+
+    // Metodo Crear clientes
+    public static void createCustomers(){
+
+        System.out.println("Ingrese el ID del Cliente");
+        idCustomer = sc.nextInt();
+        sc.nextLine();
+        System.out.println("Ingrese el Nombre");
+        nameCustomer = sc.nextLine();
+        System.out.println("Ingrese el apellido");
+        apellido = sc.nextLine();
+        System.out.println("Ingrese la dirección de residencia");
+        direccion = sc.nextLine();
+        System.out.println("Ingrese el numero de telefono");
+        telefono = sc.nextLong();
+        sc.nextLine();
+        System.out.println("Ingrese su correo electronico");
+        correo = sc.nextLine();
+    }
+
+    // Metodo Mostrar clientes
+    public static void getCustomers(){
+        System.out.println("ID Cliente: " + idCustomer + "\n" +
+                "Nombre: " + nameCustomer + "\n" +
+                "Apellido: " + apellido + "\n" +
+                "Dirección: " + direccion + "\n" +
+                "Telefono: " + telefono + "\n" +
+                "Correo: " + correo + "\n");
+    }
+
+    // Metodo Actualizar Clientes
+    public static void updateCustomers(int findIdCustomers){
+
+        if (findIdCustomers == idCustomer){
+            System.out.println("Ingrese el ID del cliente:");
+            idCustomer = sc.nextInt();
+            sc.nextLine();
+            System.out.println("Ingrese el nombre: ");
+            nameCustomer = sc.nextLine();
+            System.out.println("Ingrese el apellido:");
+            apellido = sc.nextLine();
+            System.out.println("Ingrese la dirección de residencia:");
+            direccion = sc.nextLine();
+            System.out.println("Ingrese el telefono: ");
+            telefono = sc.nextLong();
+            sc.nextLine();
+            System.out.println("Ingrese el correo electronico: ");
+            correo = sc.nextLine();
+
+        }else {
+            System.out.println("No se encontró el ID del cliente");
+        }
+    }
+
+    // Metodo Eliminar clientes
+    public static void deleteCustomers(int deleteIdCustomers){
+
+        if (deleteIdCustomers == idCustomer){
+            System.out.println(
+                            "            ID Cliente  \n" +
+                            "            Nombre  \n" +
+                            "            Apellido  \n" +
+                            "            Dirección  \n" +
+                            "            Telefono  \n" +
+                            "            Correo  ");
+            System.out.println("ID del Cliente eliminada exitosamente ");
+
+        } else {
+            System.out.println( "ID Cliente : " + id + " no encontrado");
+        }
+
+    }
     
 }
